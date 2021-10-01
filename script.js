91b542401978e50cdb6952cd41fec6acad866668
Cole Enyart <letchro@gmail.com>
2021-09-30 20:30:40 -0500

    Added function that gets api from open weather. Added function to select data needed from api

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-var searchButton = $(".btn");
-var searchCity = $(".first");
-var showApi = $(".second");
+var searchButtonEl = $("#button");
+var searchTextEl = $("#searchText");
+/* var searchCity = $(".first");
+var showApi = $(".second"); */
 
-searchButton.on("click", listCity);
+var apiKey = "2d3a1cce99e743cff7d93da2fd8af998";
+var city = "";
 
-function listCity() {
-    var test = searchButton.val();
-    console.log(test);
+
+
+
+function getApi(event) {
+    event.preventDefault();
+    city = searchTextEl.val();
+    console.log(city);
+
+    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    
+    fetch(queryUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        populateWebPage(data);
+    })
 }
 
 
 
 
-var getUserRepos = function (user) {
-var apiUrl = 'https://api.github.com/users/' + user + '/repos';
 
-fetch(apiUrl)
-    .then(function (response) {
-    if (response.ok) {
-        response.json().then(function (data) {
-        displayRepos(data, user);
-        });
-    } else {
-        alert('Error: ' + response.statusText);
-    }
-    })
-    .catch(function (error) {
-    alert('Unable to connect to GitHub');
-    });
-};
+function populateWebPage(data) {
+    var test1 = data.name + data.weather[0].icon;
+    var test2 = data.temp;
+    var test3 = data.wind.speed;
+    var test4 = data.main.humidity;
+    console.log(test1);
+    console.log("Temp: " + test2);
+    console.log("Wind: " + test3);
+    console.log("Humidity:" + test4);
+}
+
+
+searchButtonEl.on("click", getApi);
