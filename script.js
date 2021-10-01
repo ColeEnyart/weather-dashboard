2b914055994320e82a15df957f7ea302881859e9
Cole Enyart <letchro@gmail.com>
2021-09-30 21:28:45 -0500

    Updated function to output current api data onto the web page

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var searchButtonEl = $("#button");
 var searchTextEl = $("#searchText");
-/* var searchCity = $(".first");
-var showApi = $(".second"); */
+
+var currentCityEl = $(".currentCity");
+var currentIconEl = $(".img");
+var currentTempEl = $(".currentTemp");
+var currentWindEl = $(".currentWind");
+var currentHumidityEl = $(".currentHumidity");
+var currentUvEl = $(".currentUv");
 
 var apiKey = "2d3a1cce99e743cff7d93da2fd8af998";
 var city = "";
@@ -14,7 +19,7 @@ function getApi(event) {
     city = searchTextEl.val();
     console.log(city);
 
-    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + /*city*/"San Diego" + "&appid=" + apiKey + "&units=imperial";
     
     fetch(queryUrl)
     .then(function (response) {
@@ -27,18 +32,25 @@ function getApi(event) {
 }
 
 
-
+http://openweathermap.org/img/wn/10d@2x.png
 
 
 function populateWebPage(data) {
-    var test1 = data.name + data.weather[0].icon;
-    var test2 = data.temp;
+    var test1 = data.name;
+    var test1a = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+    var test2 = data.main.temp;
     var test3 = data.wind.speed;
     var test4 = data.main.humidity;
     console.log(test1);
     console.log("Temp: " + test2);
     console.log("Wind: " + test3);
-    console.log("Humidity:" + test4);
+    console.log("Humidity: " + test4);
+
+    currentCityEl.append(test1);
+    currentIconEl.attr("src", test1a);
+    currentTempEl.append("Temp: " + test2 + " &deg;F");
+    currentWindEl.append("Wind: " + test3 + " MPH");
+    currentHumidityEl.append("Humidity: " + test4 + " %");
 }
 
 
