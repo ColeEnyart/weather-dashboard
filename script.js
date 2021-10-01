0550f04e7faab68aea556f0bbfa34ad5eea9a98d
Cole Enyart <letchro@gmail.com>
2021-09-30 22:09:16 -0500

    Added classes to forecast cards, added for loop to find need data for forecast

--- script.js
+++ script.js
@@ -14,10 +14,9 @@ var city = "";
 
 
 
-function getApi(event) {
+function getCurrentApi(event) {
     event.preventDefault();
     city = searchTextEl.val();
-    console.log(city);
 
     var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + /*city*/"San Diego" + "&appid=" + apiKey + "&units=imperial";
     
@@ -27,15 +26,28 @@ function getApi(event) {
     })
     .then(function (data) {
         console.log(data);
-        populateWebPage(data);
+        populateCurrent(data);
+    })
+}
+
+function getFiveDayApi(event) {
+    event.preventDefault();
+    city = searchTextEl.val();
+
+    var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + /*city*/"San Diego" + "&appid=" + apiKey + "&units=imperial";
+    
+    fetch(queryUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        populateFiveDay(data);
     })
 }
 
 
-http://openweathermap.org/img/wn/10d@2x.png
-
-
-function populateWebPage(data) {
+function populateCurrent(data) {
     var test1 = data.name;
     var test1a = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
     var test2 = data.main.temp;
@@ -53,5 +65,35 @@ function populateWebPage(data) {
     currentHumidityEl.append("Humidity: " + test4 + " %");
 }
 
+function populateFiveDay(data) {
+    /* var test1a = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+    var test2 = data.main.temp;
+    var test3 = data.wind.speed;
+    var test4 = data.main.humidity; */
+    /* console.log("Temp: " + test2);
+    console.log("Wind: " + test3);
+    console.log("Humidity: " + test4); */
+    
+    
+    for(var i = 3; i < data.list.length; i += 8) {
+        var five1 = data.list[i].dt_txt;
+        var five2 = data.list[i].main.temp;
+        var five3 = data.list[i].wind.speed;
+        var five4 = data.list[i].main.humidity;
+        console.log(five1);
+        console.log("Temp: " + five2);
+        console.log("Wind: " + five3);
+        console.log("Humidity: " + five4);
+        
+    }
 
-searchButtonEl.on("click", getApi);
+    /* currentCityEl.append(test1);
+    currentIconEl.attr("src", test1a);
+    currentTempEl.append("Temp: " + test2 + " &deg;F");
+    currentWindEl.append("Wind: " + test3 + " MPH");
+    currentHumidityEl.append("Humidity: " + test4 + " %"); */
+}
+
+
+searchButtonEl.on("click", getCurrentApi);
+searchButtonEl.on("click", getFiveDayApi);
